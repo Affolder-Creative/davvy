010c1d32b2175d6330e10e7b2606a7a5974e6cc1
Kyle <3674851+kaffolder7@users.noreply.github.com>
2026-06-29 11:27:13 -0400

    fix broken splash screen tests

--- tests/Feature/PwaShellTest.php
+++ tests/Feature/PwaShellTest.php
@@ -21,13 +21,13 @@ class PwaShellTest extends TestCase
         $this->assertStringContainsString('name="apple-mobile-web-app-capable" content="yes"', $html);
         $this->assertStringContainsString('name="apple-mobile-web-app-status-bar-style" content="black-translucent"', $html);
 
-        $this->assertStringContainsString('images/splash/ios-splash-light-1170x2532.png', $html);
-        $this->assertStringContainsString('images/splash/ios-splash-dark-2532x1170.png', $html);
+        $this->assertStringContainsString('images/splash/ios-splash-ns-light-1170x2532.png', $html);
+        $this->assertStringContainsString('images/splash/ios-splash-ns-dark-2532x1170.png', $html);
         $this->assertStringContainsString('(prefers-color-scheme: dark) and (device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)', $html);
-        $this->assertStringContainsString('images/splash/ios-splash-light-2048x2732.png', $html);
+        $this->assertStringContainsString('images/splash/ios-splash-ns-light-2048x2732.png', $html);
         $this->assertStringContainsString('(device-width: 1024px) and (device-height: 1366px)', $html);
 
-        $this->assertFileExists(public_path('images/splash/ios-splash-light-1170x2532.png'));
-        $this->assertFileExists(public_path('images/splash/ios-splash-dark-2532x1170.png'));
+        $this->assertFileExists(public_path('images/splash/ios-splash-ns-light-1170x2532.png'));
+        $this->assertFileExists(public_path('images/splash/ios-splash-ns-dark-2532x1170.png'));
     }
 }
