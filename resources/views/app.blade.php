d4559d85990a86a6056e2b220bd7b19f33c2878b
Kyle <3674851+kaffolder7@users.noreply.github.com>
2026-06-29 10:29:13 -0400

    fix indentation

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,41 +1,41 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    @php
-        $iosStartupImages = [
-            ['width' => 320, 'height' => 568, 'ratio' => 2, 'image_width' => 640, 'image_height' => 1136],
-            ['width' => 375, 'height' => 667, 'ratio' => 2, 'image_width' => 750, 'image_height' => 1334],
-            ['width' => 414, 'height' => 736, 'ratio' => 3, 'image_width' => 1242, 'image_height' => 2208],
-            ['width' => 375, 'height' => 812, 'ratio' => 3, 'image_width' => 1125, 'image_height' => 2436],
-            ['width' => 414, 'height' => 896, 'ratio' => 2, 'image_width' => 828, 'image_height' => 1792],
-            ['width' => 414, 'height' => 896, 'ratio' => 3, 'image_width' => 1242, 'image_height' => 2688],
-            ['width' => 390, 'height' => 844, 'ratio' => 3, 'image_width' => 1170, 'image_height' => 2532],
-            ['width' => 393, 'height' => 852, 'ratio' => 3, 'image_width' => 1179, 'image_height' => 2556],
-            ['width' => 428, 'height' => 926, 'ratio' => 3, 'image_width' => 1284, 'image_height' => 2778],
-            ['width' => 430, 'height' => 932, 'ratio' => 3, 'image_width' => 1290, 'image_height' => 2796],
-            ['width' => 768, 'height' => 1024, 'ratio' => 2, 'image_width' => 1536, 'image_height' => 2048],
-            ['width' => 810, 'height' => 1080, 'ratio' => 2, 'image_width' => 1620, 'image_height' => 2160],
-            ['width' => 820, 'height' => 1180, 'ratio' => 2, 'image_width' => 1640, 'image_height' => 2360],
-            ['width' => 834, 'height' => 1112, 'ratio' => 2, 'image_width' => 1668, 'image_height' => 2224],
-            ['width' => 834, 'height' => 1194, 'ratio' => 2, 'image_width' => 1668, 'image_height' => 2388],
-            ['width' => 1024, 'height' => 1366, 'ratio' => 2, 'image_width' => 2048, 'image_height' => 2732],
-        ];
-    @endphp
+@php
+    $iosStartupImages = [
+        ['width' => 320, 'height' => 568, 'ratio' => 2, 'image_width' => 640, 'image_height' => 1136],
+        ['width' => 375, 'height' => 667, 'ratio' => 2, 'image_width' => 750, 'image_height' => 1334],
+        ['width' => 414, 'height' => 736, 'ratio' => 3, 'image_width' => 1242, 'image_height' => 2208],
+        ['width' => 375, 'height' => 812, 'ratio' => 3, 'image_width' => 1125, 'image_height' => 2436],
+        ['width' => 414, 'height' => 896, 'ratio' => 2, 'image_width' => 828, 'image_height' => 1792],
+        ['width' => 414, 'height' => 896, 'ratio' => 3, 'image_width' => 1242, 'image_height' => 2688],
+        ['width' => 390, 'height' => 844, 'ratio' => 3, 'image_width' => 1170, 'image_height' => 2532],
+        ['width' => 393, 'height' => 852, 'ratio' => 3, 'image_width' => 1179, 'image_height' => 2556],
+        ['width' => 428, 'height' => 926, 'ratio' => 3, 'image_width' => 1284, 'image_height' => 2778],
+        ['width' => 430, 'height' => 932, 'ratio' => 3, 'image_width' => 1290, 'image_height' => 2796],
+        ['width' => 768, 'height' => 1024, 'ratio' => 2, 'image_width' => 1536, 'image_height' => 2048],
+        ['width' => 810, 'height' => 1080, 'ratio' => 2, 'image_width' => 1620, 'image_height' => 2160],
+        ['width' => 820, 'height' => 1180, 'ratio' => 2, 'image_width' => 1640, 'image_height' => 2360],
+        ['width' => 834, 'height' => 1112, 'ratio' => 2, 'image_width' => 1668, 'image_height' => 2224],
+        ['width' => 834, 'height' => 1194, 'ratio' => 2, 'image_width' => 1668, 'image_height' => 2388],
+        ['width' => 1024, 'height' => 1366, 'ratio' => 2, 'image_width' => 2048, 'image_height' => 2732],
+    ];
+@endphp
     <meta charset="UTF-8">
     <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml">
     <link rel="alternate icon" href="{{ asset('favicon.ico') }}" sizes="any">
     <link rel="apple-touch-icon" href="{{ asset('images/icons/apple-touch-icon-180.png') }}">
-    @foreach ($iosStartupImages as $startupImage)
-        @foreach (['portrait', 'landscape'] as $orientation)
-            @php
-                $splashWidth = $orientation === 'portrait' ? $startupImage['image_width'] : $startupImage['image_height'];
-                $splashHeight = $orientation === 'portrait' ? $startupImage['image_height'] : $startupImage['image_width'];
-            @endphp
-            @foreach (['light', 'dark'] as $scheme)
+@foreach ($iosStartupImages as $startupImage)
+@foreach (['portrait', 'landscape'] as $orientation)
+@php
+    $splashWidth = $orientation === 'portrait' ? $startupImage['image_width'] : $startupImage['image_height'];
+    $splashHeight = $orientation === 'portrait' ? $startupImage['image_height'] : $startupImage['image_width'];
+@endphp
+@foreach (['light', 'dark'] as $scheme)
     <link rel="apple-touch-startup-image" href="{{ asset("images/splash/ios-splash-{$scheme}-{$splashWidth}x{$splashHeight}.png") }}" media="(prefers-color-scheme: {{ $scheme }}) and (device-width: {{ $startupImage['width'] }}px) and (device-height: {{ $startupImage['height'] }}px) and (-webkit-device-pixel-ratio: {{ $startupImage['ratio'] }}) and (orientation: {{ $orientation }})">
-            @endforeach
-        @endforeach
-    @endforeach
+@endforeach
+@endforeach
+@endforeach
     <link rel="manifest" href="{{ asset('manifest.webmanifest') }}">
     <meta name="theme-color" content="#00786f">
     <meta name="apple-mobile-web-app-capable" content="yes">
